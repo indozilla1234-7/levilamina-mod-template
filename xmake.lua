170128248cfd21eba8f20e93a86211113568fc1b
indozilla1234-7 <Ethanzilla1234@hotmail.com>
2026-03-19 09:24:48 +0000

    Fix xmake path in GitHub Actions

--- xmake.lua
+++ xmake.lua
@@ -1,36 +1,41 @@
 add_rules("mode.debug", "mode.release")
 
--- Use the official repository
 add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
 
--- Define dependencies
--- Note: 'levibuildscript' is essential for the rules below to work
-add_requires("levilamina 1.9.8") 
+option("target_type")
+    set_default("server")
+    set_showmenu(true)
+    set_values("server", "client")
+option_end()
+
+-- add_requires("levilamina x.x.x") for a specific version
+-- add_requires("levilamina develop") to use develop version
+-- please note that you should add bdslibrary yourself if using dev version
+add_requires("levilamina", {configs = {target_type = get_config("target_type")}})
+
 add_requires("levibuildscript")
 
-target("my-mod")
-    -- Rules for building a LeviLamina mod
+if not has_config("vs_runtime") then
+    set_runtimes("MD")
+end
+
+target("my-mod") -- Change this to your mod name.
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    
-    -- Library and Language Settings
+    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+    add_defines("NOMINMAX", "UNICODE")
     add_packages("levilamina")
+    set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
-    set_languages("c++20") -- Stick to C++20 to avoid the MSVC 14.44 VectorBase bug
+    set_languages("c++20")
     set_symbols("debug")
-    set_exceptions("none")
-
-    -- Compiler Flags: Added /permissive- to help with the 'formatter' errors
-    if is_plat("windows") then
-        add_cxflags("/EHa", "/utf-8", "/W4", "/permissive-")
-        add_defines("NOMINMAX", "UNICODE")
-    end
-
-    -- CLEAN FILE SELECTION
-    -- Only include your actual source files. 
-    -- Do NOT manually include or exclude 'll' or 'mc' folders here.
     add_headerfiles("src/**.h")
     add_files("src/**.cpp")
-
-    -- Include Directories
     add_includedirs("src")
+    if is_config("target_type", "server") then
+    --  add_includedirs("src-server")
+    --  add_files("src-server/**.cpp")
+    else
+    --  add_includedirs("src-client")
+    --  add_files("src-client/**.cpp")
+    end
