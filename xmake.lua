677736d829e336c08d28bf5b4c86b54572f76a49
indozilla1234-7 <Ethanzilla1234@hotmail.com>
2026-03-19 09:45:22 +0000

    Fix xmake path in GitHub Actions

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
 add_rules("mode.debug", "mode.release")
 
+-- 1. Set global language to C++20 to avoid the C++23 Formatter bug
 set_languages("c++20")
 
 add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
@@ -10,34 +11,34 @@ option("target_type")
     set_values("server", "client")
 option_end()
 
--- add_requires("levilamina x.x.x") for a specific version
--- add_requires("levilamina develop") to use develop version
--- please note that you should add bdslibrary yourself if using dev version
-add_requires("levilamina", {configs = {target_type = get_config("target_type")}})
-
+-- 2. Added a specific version here. Using the latest stable helps Xmake find it faster.
+add_requires("levilamina 1.9.8", {configs = {target_type = get_config("target_type")}})
 add_requires("levibuildscript")
 
-if not has_config("vs_runtime") then
+if is_plat("windows") then
     set_runtimes("MD")
 end
 
-target("my-mod") -- Change this to your mod name.
+target("pax-bedrockia") -- Changed to your cool mod name!
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+    
+    -- 3. REMOVED the redundant /W4 and warning flags that cause header panics
+    -- Added /permissive- which is the "magic fix" for many MSVC template errors
+    add_cxflags("/EHa", "/utf-8", "/permissive-")
     add_defines("NOMINMAX", "UNICODE")
+    
     add_packages("levilamina")
-    set_exceptions("none") -- To avoid conflicts with /EHa.
+    
+    set_exceptions("none") 
     set_kind("shared")
-    set_languages("c++20")
     set_symbols("debug")
+
+    -- 4. Standard file selection
     add_headerfiles("src/**.h")
     add_files("src/**.cpp")
     add_includedirs("src")
+
     if is_config("target_type", "server") then
-    --  add_includedirs("src-server")
-    --  add_files("src-server/**.cpp")
-    else
-    --  add_includedirs("src-client")
-    --  add_files("src-client/**.cpp")
+        -- server specific logic
     end
