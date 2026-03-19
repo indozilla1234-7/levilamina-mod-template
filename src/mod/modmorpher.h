864bc4657d562a40076f9b76f9c3cd38843af136
indozilla1234-7 <Ethanzilla1234@hotmail.com>
2026-03-19 08:12:18 -0400

    Update modmorpher.h

--- src/mod/modmorpher.h
+++ src/mod/modmorpher.h
@@ -1,68 +1,63 @@
 #pragma once
 
-#include "jni.h"
-#include <string>
-#include <map>
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#include <atomic>
 #include <functional>
+#include <map>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
 #include <vector>
 
-// Bedrock type includes
-#include "mc/math/vector/Vecs.h"
-#include "mc/world/level/block/Block.h"
-#include "mc/world/level/BlockSource.h"
-#include "mc/world/level/BlockPos.h"
+#include <jni.h>
+
+namespace ll {
+    namespace api {
+        class Logger;
+    }
+}
+
+namespace mc {
+    namespace world {
+        namespace actor {
+            class Actor;
+            namespace player {
+                class Player;
+            }
+        }
+        namespace level {
+            class Level;
+            class BlockSource;
+            namespace block {
+                class Block;
+            }
+        }
+    }
+}
 
 namespace modmorpher {
 
 // ============================================================================
-// BEDROCK SYMBOL RESOLVER
+// Simple Vec3 / BlockPos helpers
 // ============================================================================
+struct Vec3 {
+    float x, y, z;
+};
 
-class BedrockSymbolResolver {
-public:
-    using ActorSetPos       = void(*)(void* actor, const void* vec3);
-    using ActorGetPos       = void(*)(void* actor, void* outVec3);
-    using BlockSetType      = void(*)(void* blockSource, int x, int y, int z, const void* block);
-    using ActorAddTag       = void(*)(void* actor, const char* tag);
-    using ActorSetAttribute = void(*)(void* actor, const void* attribute, float value);
-    using CommandExecute    = int(*)(void* level, const char* command);
-    using BlockCreate        = void*(*)(const char* blockName);
-    using DimensionGetBlock  = const Block*(*)(BlockSource*, const BlockPos&);
-
-    static bool initialize();
-
-    static ActorSetPos       getActorSetPos();
-    static ActorGetPos       getActorGetPos();
-    static BlockSetType      getBlockSetType();
-    static DimensionGetBlock getDimensionGetBlock();
-    static ActorAddTag       getActorAddTag();
-    static ActorSetAttribute getActorSetAttribute();
-    static CommandExecute    getCommandExecute();
-    static BlockCreate       getBlockCreate();
-
-    static void* resolveSymbol(const char* name);
-
-private:
-    static ActorSetPos       actorSetPos;
-    static ActorGetPos       actorGetPos;
-    static BlockSetType      blockSetType;
-    static DimensionGetBlock dimensionGetBlock;
-    static ActorAddTag       actorAddTag;
-    static ActorSetAttribute actorSetAttribute;
-    static CommandExecute    commandExecute;
-    static BlockCreate       blockCreate;
-    static bool              resolved;
+struct BlockPos {
+    int x, y, z;
 };
 
 // ============================================================================
-// JNI THREAD MANAGER
+// JNI thread manager
 // ============================================================================
-
 class JNIThreadManager {
 public:
-    static void setJVM(JavaVM* vm);
-    static bool ensureAttached();
-    static void detachCurrentThread();
+    static void   setJVM(JavaVM* vm);
     static JNIEnv* getEnv();
 
     class ThreadGuard {
@@ -71,18 +66,55 @@ public:
         ~ThreadGuard();
         JNIEnv* getEnv();
     private:
-        bool wasAttached;
+        bool wasAttached{false};
     };
 
 private:
-    static JavaVM* jvm;
-    thread_local static JNIEnv* threadEnv;
+    static bool   ensureAttached();
+    static void   detachCurrentThread();
+
+    static JavaVM*              jvm;
+    static thread_local JNIEnv* threadEnv;
 };
 
 // ============================================================================
-// BLOCK STATE MAPPER
+// Bedrock symbol resolver
 // ============================================================================
+class BedrockSymbolResolver {
+public:
+    using ActorSetPos       = void(*)(void*, Vec3 const*);
+    using ActorGetPos       = Vec3 const&(*)(void*, Vec3*);
+    using BlockSetType      = bool(*)(void*, int, int, int, void*);
+    using ActorAddTag       = void(*)(void*, std::string const&);
+    using ActorSetAttribute = void(*)(void*, void const&, float);
+    using CommandExecute    = void(*)(mc::world::level::Level&, std::string const&, class CommandOutput&);
+    using BlockCreate       = void*(*)(char const*);
 
+    static bool initialize();
+    static void* resolveSymbol(char const* name);
+
+    static ActorSetPos       getActorSetPos();
+    static ActorGetPos       getActorGetPos();
+    static BlockSetType      getBlockSetType();
+    static ActorAddTag       getActorAddTag();
+    static ActorSetAttribute getActorSetAttribute();
+    static CommandExecute    getCommandExecute();
+    static BlockCreate       getBlockCreate();
+
+private:
+    static ActorSetPos       actorSetPos;
+    static ActorGetPos       actorGetPos;
+    static BlockSetType      blockSetType;
+    static ActorAddTag       actorAddTag;
+    static ActorSetAttribute actorSetAttribute;
+    static CommandExecute    commandExecute;
+    static BlockCreate       blockCreate;
+    static bool              resolved;
+};
+
+// ============================================================================
+// Block state mapper
+// ============================================================================
 class BlockStateMapper {
 public:
     struct ForgeBlockState {
@@ -95,27 +127,26 @@ public:
         std::map<std::string, std::string> properties;
     };
 
-    static bool loadMappings(const std::string& file);
-    static BedrockBlockState forgeToBedrockBlockState(const ForgeBlockState&);
-    static ForgeBlockState  bedrockToForgeBlockState(const BedrockBlockState&);
-    static std::string      mapBlockName(const std::string& forgeName);
+    static bool              loadMappings(std::string const& path);
+    static std::string       mapBlockName(std::string const& forgeName);
+    static BedrockBlockState forgeToBedrockBlockState(ForgeBlockState const& fs);
+    static ForgeBlockState   bedrockToForgeBlockState(BedrockBlockState const& bs);
 
 private:
-    static std::map<std::string, std::string> blockNameMappings;
+    static std::map<std::string, std::string>                       blockNameMappings;
     static std::map<std::string, std::map<std::string, std::string>> propertyMappings;
 };
 
 // ============================================================================
-// ENTITY TRACKER
+// Entity tracker
 // ============================================================================
-
 class EntityTracker {
 public:
-    static void    registerEntity(JNIEnv* env, jobject javaEntity, void* bedrockActor);
-    static void*   getBedrockActor(JNIEnv* env, jobject javaEntity);
+    static void   registerEntity(JNIEnv* env, jobject javaEntity, void* bedrockActor);
+    static void*  getBedrockActor(JNIEnv* env, jobject javaEntity);
     static jobject getJavaEntity(void* bedrockActor);
-    static void    unregisterEntity(JNIEnv* env, jobject javaEntity);
-    static bool    hasEntity(JNIEnv* env, jobject javaEntity);
+    static void   unregisterEntity(JNIEnv* env, jobject javaEntity);
+    static bool   hasEntity(JNIEnv* env, jobject javaEntity);
 
 private:
     static jlong getEntityId(JNIEnv* env, jobject javaEntity);
@@ -126,94 +157,62 @@ private:
 };
 
 // ============================================================================
-// NATIVE SHADOW ADAPTER
+// Native shadow adapter
 // ============================================================================
-
 class NativeShadowAdapter {
 public:
-    static bool registerNativeMethods(JNIEnv* env, const std::string& pkg);
+    static bool registerNativeMethods(JNIEnv* env, std::string const& pkg);
 
+private:
     static void JNICALL nativeEntitySetPos(JNIEnv* env, jobject entity, jdouble x, jdouble y, jdouble z);
     static jobject JNICALL nativeEntityGetPos(JNIEnv* env, jobject entity);
     static void JNICALL nativeEntityAddTag(JNIEnv* env, jobject entity, jstring tag);
-
     static jboolean JNICALL nativeBlockSetBlock(JNIEnv* env, jobject, jobject blockPos, jobject blockState, jint flags);
-    static jobject  JNICALL nativeBlockGetBlock(JNIEnv* env, jobject, jobject blockPos);
+    static jobject JNICALL nativeBlockGetBlock(JNIEnv* env, jobject, jobject blockPos);
+    static jobject JNICALL nativeItemUse(JNIEnv* env, jobject, jobject, jobject, jint);
 
-    static jobject JNICALL nativeItemUse(JNIEnv*, jobject, jobject, jobject, jint);
-
-private:
     static JNINativeMethod nativeMethods[];
-    static int nativeMethodCount;
+    static int             nativeMethodCount;
 };
 
 // ============================================================================
-// BEDROCK POINTER HELPER
+// Bedrock pointer helper
 // ============================================================================
-
 class BedrockPointerHelper {
 public:
-    struct BlockPos { int x, y, z; };
+    static Vec3        makeVec3(double x, double y, double z);
+    static jdoubleArray vec3ToJDoubleArray(JNIEnv* env, Vec3 const& v);
 
-    // Use Bedrock's Vec3
-    static Vec3 makeVec3(double x, double y, double z);
-    static jdoubleArray vec3ToJDoubleArray(JNIEnv* env, const Vec3& vec);
-
-    static BlockPos extractBlockPos(JNIEnv* env, jobject blockPosObj);
-    static jobject createBlockPos(JNIEnv* env, int x, int y, int z);
+    static BlockPos extractBlockPos(JNIEnv* env, jobject obj);
+    static jobject  createBlockPos(JNIEnv* env, int x, int y, int z);
 };
 
 // ============================================================================
-// EVENT FORWARDER
+// Forge event forwarder
 // ============================================================================
-
 class ForgeEventForwarder {
 public:
-    using PlayerEventHandler = std::function<void(const std::string&)>;
-    using BlockEventHandler  = std::function<void(int, int, int, const std::string&)>;
-
-    static void registerLeviLaminaHooks();
-
-    static void onPlayerJoin(PlayerEventHandler);
-    static void onPlayerLeave(PlayerEventHandler);
-    static void onBlockBreak(BlockEventHandler);
-    static void onBlockPlace(BlockEventHandler);
-
-    static void forwardPlayerJoinEvent(const std::string&);
-    static void forwardPlayerLeaveEvent(const std::string&);
-    static void forwardBlockBreakEvent(int x, int y, int z, const std::string&);
-    static void forwardBlockPlaceEvent(int x, int y, int z, const std::string& block, const std::string& player);
-
-    static void registerForgeEventListener(JNIEnv* env, const std::string& eventClass, jobject listener);
-
-private:
-    static std::vector<PlayerEventHandler> playerJoinHandlers;
-    static std::vector<PlayerEventHandler> playerLeaveHandlers;
-    static std::vector<BlockEventHandler>  blockBreakHandlers;
-    static std::vector<BlockEventHandler>  blockPlaceHandlers;
-    static std::vector<jobject>            registeredForgeListeners;
-};
-
-// ============================================================================
-// MODMORPHER MANAGER
-// ============================================================================
-
-class ModMorpher {
-public:
-    static bool initialize(JavaVM* jvm, JNIEnv* env);
+    static void init();
     static void shutdown();
 
-    static bool loadForgeMod(const std::string& jarPath);
-    static bool unloadForgeMod(const std::string& modId);
+    static void forwardActorHurt(mc::world::actor::Actor& actor, int damage);
+    static void forwardPlayerDeath(mc::world::actor::player::Player& player);
+    static void forwardMobDeath(mc::world::actor::Actor& mob);
+    static void forwardBlockBreak(mc::world::actor::player::Player& player, BlockPos const& pos);
+    static void forwardPlayerChat(mc::world::actor::player::Player& player, std::string const& msg);
+    static void forwardPlayerJoin(mc::world::actor::player::Player& player);
 
-    static std::vector<std::string> getLoadedMods();
-    static bool isInitialized();
+    static void clearListeners(JNIEnv* env);
 
 private:
-    static bool initialized;
-    static JavaVM* cachedJVM;
-    static JNIEnv* cachedEnv;
-    static std::vector<std::string> loadedMods;
+    static std::vector<jobject> registeredForgeListeners;
 };
+
+// ============================================================================
+// Global JNI queue helpers
+// ============================================================================
+void enqueueJNICall(std::string desc, std::function<void(JNIEnv*)> fn);
+void startJNIWorker();
+void stopJNIWorker();
 
 } // namespace modmorpher
